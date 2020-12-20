18e8db5e7d91218f1a560ed55c0302b38c6c5b12
Yuri Savkov <yury.saukou@gmail.com>
2020-12-20 14:52:01 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,55 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = expr.split('')
+    const newArr = [];
+
+    for (let i = 0; i < (expr.length / 10); i++) {
+        newArr.push(arr.slice(0, 10).join(''));
+        arr.splice(0, 10)
+    }
+
+    const sliced = newArr.map((e) => {
+        if (e[0] === '*') {
+            return ' '
+        }
+
+        const eSpl = e.split('');
+        const res = [];
+
+        for (let i = 0; i < (e.length / 2); i++) {
+            res.push(eSpl.slice(0, 2).join(''));
+            eSpl.splice(0, 2)
+        }
+
+        return res
+    }).map((e) => {
+        if (e === ' ') {
+            return ' '
+        }
+        const res = []
+        for (let x of e) {
+            switch (x) {
+                case '00':
+                    break;
+                case '10':
+                    res.push('.');
+                    break;
+                case '11':
+                    res.push('-');
+                    break;
+            }
+        }
+        return res.join('')
+    }).map((e) => {
+        return MORSE_TABLE[e];
+    }).join('');
+    
+    return sliced
 }
 
 module.exports = {
